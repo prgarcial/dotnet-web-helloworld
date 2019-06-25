2362e9678f7b823bda9a075b2bc039ba610739d0
Russ McKendrick <russ@mckendrick.io>
2019-06-25 17:11:06 +0000

    update

--- Startup.cs
+++ Startup.cs
@@ -27,7 +27,7 @@ namespace web
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello Again!");
+                await context.Response.WriteAsync("Hello There!");
             });
         }
     }
